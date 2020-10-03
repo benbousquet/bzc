cdf637a6f01cde669b0bd3c27ac1e9c565455b43
benbousquet <bbousque@asu.edu>
2020-10-02 22:12:25 -0700

    feat(server): setup express and socket.io

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello there sir");
+import express from "express";
+import http from "http";
+import ioserver from "socket.io";
+
+const app = express();
+const server: http.Server = new http.Server(app);
+const io: SocketIO.Server = ioserver(server);
+const port = 3001;
+
+io.on("connection", (socket) => {
+  console.log("he joined :D");
+  socket.on("disconnect", () => console.log("he left :("));
+});
+
+server.listen(port);
